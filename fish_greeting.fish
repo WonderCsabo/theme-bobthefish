c753efeab84fcf70e28c8dd82ef7a14bbc6319ed
Csaba Kozak <kozakcsabi@gmail.com>
2016-05-30 10:51:04 +0200

    Remove greeting

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,6 +1,6 @@
 function fish_greeting -d "what's up, fish?"
-  set_color $fish_color_autosuggestion
-  uname -npsr
-  uptime
-  set_color normal
+  # set_color $fish_color_autosuggestion
+  # uname -npsr
+  # uptime
+  # set_color normal
 end
